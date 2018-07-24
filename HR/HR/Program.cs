f448161fc8f35c5690c221e6d4c9a406f9fc6672
erinliu <erinliu@localhost.com>
2018-07-24 13:54:28 +0800

    这是一个尝试

--- HR/HR/Program.cs
+++ HR/HR/Program.cs
@@ -17,6 +17,8 @@ namespace HR
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //这是一个test
+
         }
     }
 }
